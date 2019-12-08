2c85c1906ad54fd8b8eb3a4f3608359fc7328835
Gina Graci <ginagraci92@gmail.com>
2019-12-08 03:19:39 +0000

    start building out cli file

---
+++ lib/fun_dog_facts/cli.rb
@@ -0,0 +1,10 @@
+# Our CLI Controller
+
+class FunDogFacts::CLI 
+  
+  def call
+    puts "Welcome to the Fun Dog Facts CLI gem! Get ready to learn some cool stuff about our doggo friends!"
+  end
+  
+  
+end 
