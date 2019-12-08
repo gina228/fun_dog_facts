c531119c247e246810ed41a964b4a9fa32f3d107
Gina Graci <ginagraci92@gmail.com>
2019-12-08 04:11:10 +0000

    building out cli file

--- lib/fun_dog_facts.rb
+++ lib/fun_dog_facts.rb
@@ -1,2 +1,3 @@
 require_relative "./fun_dog_facts/version"
 require_relative "./fun_dog_facts/cli"
+
--- lib/fun_dog_facts/cli.rb
+++ lib/fun_dog_facts/cli.rb
@@ -3,8 +3,53 @@
 class FunDogFacts::CLI 
   
   def call
-    puts "Welcome to the Fun Dog Facts CLI gem! Get ready to learn some cool stuff about our doggo friends!"
+    welcome
+    puts ""
+    list_fact_titles
+    fun_fact
   end
   
+  def welcome
+    puts "Welcome to the Fun Dog Facts CLI gem! Get ready to learn some cool stuff about our doggo friends, no matter how smol or large!"
+  end
+  
+  def list_fact_titles
+    puts "Type in a number that corresponds with a surprise fun fact you want to see, type 'list' to get back to the list of facts at any time, OR type 'exit' to leave the program"
+    puts ""
+    puts "1. A fun fact!"
+    puts "2. Another fun fact!"
+    puts "3. Yet another fun fact!"
+    puts "4. Oh look, a fun fact!"
+    puts "5. Fun facts are fun!"
+    puts ""
+    
+  end
+  
+  def fun_fact
+    input = nil 
+    while input != "exit"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts ""
+      when "2"
+        puts ""
+      when "3"
+        puts ""
+      when "4"
+        puts ""
+      when "5"
+        puts ""
+      when "list"
+        list_fact_titles
+      when "exit"
+        goodbye
+      end
+    end
+  end
+  
+  def goodbye
+    puts "We hope you have a PAWSITIVELY awesome day! Bye!"
+  end
   
 end 
